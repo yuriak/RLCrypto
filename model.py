2a041e0f50e60c3c7960835180b2214a9db1b0ee
yuriak <yuriak1911@gmail.com>
2018-06-25 13:34:02 +1000

    Added Directed reinforcement learning model

--- model.py
+++ model.py
@@ -289,3 +289,114 @@ class RPG_Crypto_portfolio(object):
             os.mkdir(model_path)
         model_file = model_path + '/model'
         self.saver.save(self.session, model_file)
+
+
+class DRL_Crypto_portfolio(object):
+    def __init__(self, feature_number, action_size=1, c=1e-5, hidden_units_number=[128, 64], learning_rate=0.001):
+        tf.reset_default_graph()
+        self.s = tf.placeholder(dtype=tf.float32, shape=[None, feature_number], name='s')
+        self.d = tf.placeholder(dtype=tf.float32, shape=[None, action_size - 1], name='d')
+        self.s_buffer = []
+        self.d_buffer = []
+        self.c = c
+        self.action_size = action_size
+        self.dropout_keep_prob = tf.placeholder(dtype=tf.float32, shape=[], name='dropout_keep_prob')
+        with tf.variable_scope('rnn_encoder', initializer=tf.contrib.layers.xavier_initializer(uniform=True), regularizer=tf.contrib.layers.l2_regularizer(0.01)):
+            #             cell=self._add_GRU(units_number=128,keep_prob=self.dropout_keep_prob)
+            cells = self._add_GRUs(units_number=[128, action_size], activation=[tf.nn.relu, tf.nn.relu])
+            self.rnn_input = tf.expand_dims(self.s, axis=0)
+            self.rnn_output, _ = tf.nn.dynamic_rnn(inputs=self.rnn_input, cell=cells, dtype=tf.float32)
+            #             self.rnn_output=tf.contrib.layers.layer_norm(self.rnn_output)
+            self.a_prob = tf.unstack(self.rnn_output, axis=0)[0]
+
+        # with tf.variable_scope('supervised',initializer=tf.contrib.layers.xavier_initializer(uniform=True), regularizer=tf.contrib.layers.l2_regularizer(0.01)):
+        #             self.state_predict = self._add_dense_layer(inputs=self.rnn_output, output_shape=hidden_units_number, drop_keep_prob=self.dropout_keep_prob, act=tf.nn.relu, use_bias=True)
+        #             self.state_predict = self._add_dense_layer(inputs=self.rnn_output, output_shape=[feature_number], drop_keep_prob=self.dropout_keep_prob, act=None, use_bias=True)
+        #             self.state_loss=tf.losses.mean_squared_error(self.state_predict,self.s_next)
+        
+        with tf.variable_scope('direct_RL', initializer=tf.contrib.layers.xavier_initializer(uniform=True), regularizer=tf.contrib.layers.l2_regularizer(0.01)):
+            #             self.rnn_output=tf.stop_gradient(self.rnn_output)
+            #             self.a_prob = self._add_dense_layer(inputs=self.rnn_output, output_shape=hidden_units_number+[action_size], drop_keep_prob=self.dropout_keep_prob, act=tf.nn.relu, use_bias=True)
+            #             self.a_prob = self._add_dense_layer(inputs=self.a_prob, output_shape=, drop_keep_prob=self.dropout_keep_prob, act=None, use_bias=True)
+            self.a_out = tf.nn.softmax(self.a_prob, axis=-1)
+            self.a_out = tf.concat((tf.zeros(dtype=tf.float32, shape=[1, self.action_size]), self.a_out), axis=0)
+            self.reward = tf.reduce_sum(self.d * self.a_out[:-1, :-1] - self.c * tf.abs(self.a_out[1:, :-1] - self.a_out[:-1, :-1]), axis=1)
+            self.total_reward = tf.reduce_sum(self.reward)
+            self.mean_reward = tf.reduce_mean(self.reward)
+        
+        with tf.variable_scope('train'):
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            self.train_op = optimizer.minimize(-self.mean_reward)
+        self.init_op = tf.global_variables_initializer()
+        self.session = tf.Session()
+        self.saver = tf.train.Saver()
+    
+    def init_model(self):
+        self.session.run(self.init_op)
+    
+    def _add_dense_layer(self, inputs, output_shape, drop_keep_prob, act=tf.nn.relu, use_bias=True):
+        output = inputs
+        for n in output_shape:
+            output = tf.layers.dense(output, n, activation=act, use_bias=use_bias)
+            output = tf.nn.dropout(output, drop_keep_prob)
+        return output
+    
+    def _add_GRU(self, units_number, activation=tf.nn.relu, keep_prob=1.0):
+        cell = tf.contrib.rnn.LSTMCell(units_number, activation=activation)
+        cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=keep_prob)
+        return cell
+    
+    def _add_GRUs(self, units_number, activation, keep_prob=1.0):
+        cells = tf.contrib.rnn.MultiRNNCell(cells=[self._add_GRU(units_number=n, activation=a) for n, a in zip(units_number, activation)])
+        return cells
+    
+    def _add_gru_cell(self, units_number, activation=tf.nn.relu):
+        return tf.contrib.rnn.GRUCell(num_units=units_number, activation=activation)
+    
+    def train(self, drop=0.85):
+        #         np.random.shuffle(random_index)
+        feed = {
+            self.s: np.array(self.s_buffer),
+            self.d: np.array(self.d_buffer),
+            self.dropout_keep_prob: drop
+        }
+        self.session.run([self.train_op], feed_dict=feed)
+    
+    def restore_buffer(self):
+        self.s_buffer = []
+        self.d_buffer = []
+    
+    def save_current_state(self, s, d):
+        self.s_buffer.append(s)
+        self.d_buffer.append(d)
+    
+    def trade(self, train=False, drop=1.0, prob=False):
+        feed = {
+            self.s: np.array(self.s_buffer),
+            self.dropout_keep_prob: drop
+        }
+        a_prob = self.session.run([self.a_out], feed_dict=feed)
+        a_prob = a_prob[-1][-1].flatten()
+        return a_prob
+    
+    #         if train:
+    #             a_indices = np.arange(a_prob.shape[0])
+    #             target_index=np.random.choice(a_indices, p=a_prob)
+    #             a=np.zeros(a_prob.shape[0])
+    #             a[target_index]=1.0
+    #             return a
+    #         else:
+    #             if prob:
+    #                 return a_prob
+    #             target_index=np.argmax(a_prob)
+    #             a=np.zeros(a_prob.shape[0])
+    #             a[target_index]=1.0
+    #             return a
+    def load_model(self, model_path='./RPGModel'):
+        self.saver.restore(self.session, model_path + '/model')
+    
+    def save_model(self, model_path='./RPGModel'):
+        if not os.path.exists(model_path):
+            os.mkdir(model_path)
+        model_file = model_path + '/model'
+        self.saver.save(self.session, model_file)
